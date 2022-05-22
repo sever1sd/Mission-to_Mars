fff8c760a2da66defe13e536d2146550e5062c98
Shane Sever <sever.shane@gmail.com>
2022-05-22 12:49:10 -0400

    adding updated code

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
+
 
 @app.route("/scrape")
 def scrape():
@@ -20,5 +22,6 @@ def scrape():
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
+# Define Main Behavior
 if __name__ == "__main__":
-   app.run()
+    app.run()
